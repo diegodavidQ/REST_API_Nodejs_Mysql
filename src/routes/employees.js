a09739b95be68457051159fda48643fee8b18b46
diegodavidq <diegodavidq@outlook.com>
2021-08-16 23:19:12 -0500

    CRUD finalizado

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -27,15 +27,39 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
     const { id, name, salary } = req.body;
+    console.log(req.body);
+
     const query = `
-        SET @id = ?;
-        SET @name = ?;
-        SET @salary = ?;
-        CALL employeeAddOrEdit(@id, @name, @salary);
+        CALL employeeAddOrEdit(?, ?, ?);
     `;
     mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
         if(!err){
             res.json({Status: 'Employeed Saved'});
+        }else {
+            console.log(err); 
+        }
+    });
+});
+
+router.put('/:id', (req, res) => {
+    const { name, salary } = req.body;
+    const { id } = req.params;
+
+    const query = 'CALL employeeAddOrEdit(?, ?, ?)';
+    mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+        if(!err){
+            res.json({Status: 'Employeed Updated'});
+        }else {
+            console.log(err); 
+        }
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    mysqlConnection.query('DELETE FROM employees WHERE id = ?', [id], (err, rows, fields) => {
+        if(!err){
+            res.json({Status: 'Employeed Deleted'});
         }else {
             console.log(err); 
         }
