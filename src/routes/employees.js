643cc399aebe307d6bba69b29b29c7a0b476f9d2
diegodavidq <diegodavidq@outlook.com>
2021-08-16 22:51:05 -0500

    procedure mysql

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -10,7 +10,36 @@ router.get('/', (req, res) => {
         } else{
             console.log(err);
         }
-    })
-})
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    
+    mysqlConnection.query('SELECT * FROM employees WHERE id = ?', [id], (err, rows, fields) => {
+        if(!err){
+            res.json(rows[0]);
+        } else{
+            console.log(err);
+        }
+    });
+});
+
+router.post('/', (req, res) => {
+    const { id, name, salary } = req.body;
+    const query = `
+        SET @id = ?;
+        SET @name = ?;
+        SET @salary = ?;
+        CALL employeeAddOrEdit(@id, @name, @salary);
+    `;
+    mysqlConnection.query(query, [id, name, salary], (err, rows, fields) => {
+        if(!err){
+            res.json({Status: 'Employeed Saved'});
+        }else {
+            console.log(err); 
+        }
+    });
+});
 
 module.exports = router;
